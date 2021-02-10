309fe6d6032a9894809d844e93a534bfc80e126b
aprakhotski <74299034+aprakhotski@users.noreply.github.com>
2021-02-10 22:06:03 +0300

    Update and rename stroop.php to Task2/stroop.php

--- Task2/stroop.php
+++ Task2/stroop.php
@@ -17,8 +17,7 @@ for ($i=0; $i<5; $i++){
 	$tmpWords = $words;
 	
 	for ($j=0; $j<5; $j++){						
-		$keyWord = array_rand($tmpWords, 1);
-			
+		$keyWord = array_rand($tmpWords, 1);			
 		do 
 		$keyColor = array_rand($words, 1);
 		while ($words[$keyColor] == $tmpWords[$keyWord]);		
