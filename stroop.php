ba1c182a63ade11105e7d9648f92262fd6f1bfc3
aprakhotski <74299034+aprakhotski@users.noreply.github.com>
2021-02-10 22:05:20 +0300

    Add files via upload

---
+++ stroop.php
@@ -0,0 +1,31 @@
+<?php
+
+$words = [
+	'red',
+	'blue',
+	'green',
+	'yellow', 
+	'lime', 
+	'magenta', 
+	'black', 
+	'gold', 
+	'gray', 
+	'tomato'
+	];
+	
+for ($i=0; $i<5; $i++){	
+	$tmpWords = $words;
+	
+	for ($j=0; $j<5; $j++){						
+		$keyWord = array_rand($tmpWords, 1);
+			
+		do 
+		$keyColor = array_rand($words, 1);
+		while ($words[$keyColor] == $tmpWords[$keyWord]);		
+		
+		echo "<span style='color:$words[$keyColor]; font-size:35px;
+			 text-shadow: 0 0 1px black;'> $tmpWords[$keyWord] </span>";
+		array_splice($tmpWords, $keyWord, 1);
+	}
+	echo "<p></p>";
+}
