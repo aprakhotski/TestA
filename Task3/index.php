a8af68235796eeae712031dcf8c9754b20118de4
aprakhotski <74299034+aprakhotski@users.noreply.github.com>
2021-02-10 22:24:18 +0300

    Update index.php

--- Task3/index.php
+++ Task3/index.php
@@ -11,7 +11,7 @@
 	<body >
 		<div class="container">
 			<div class="row">
-				<div >	
+				<div>	
 					<h1>Банкомат</h1>					 
 					<form action="form.php" method="post">
 						<p>Номинал в наличии:</p>
